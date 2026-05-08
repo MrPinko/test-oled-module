f9466694e78657a75d724708c29260ffe2d69466
Federico <federico.rosa@dierresoftware.com>
2026-05-08 16:59:53 +0200

    init

---
+++ src/behaviors/oled_toggle.c
@@ -0,0 +1,45 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/display.h>
+#include <zmk/behavior.h>
+
+#define DT_DRV_COMPAT zmk_behavior_oled_toggle
+
+static bool oled_on = true;
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    const struct device *display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+
+    if (!device_is_ready(display)) {
+        return -ENODEV;
+    }
+
+    oled_on = !oled_on;
+
+    if (oled_on) {
+        display_blanking_off(display);
+    } else {
+        display_blanking_on(display);
+    }
+
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static const struct behavior_driver_api behavior_oled_toggle_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
+};
+
+#define OLED_TOGGLE_INST(n)                                          \
+    BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, NULL, NULL,              \
+                            POST_KERNEL,                             \
+                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,     \
+                            &behavior_oled_toggle_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(OLED_TOGGLE_INST)
